f80482f98bc05eefe52e2b547042df4cc73e69ab
Niels Heissel <niels.heissel@konzepthaus-ws.de>
2018-01-15 23:14:01 +0100

    First initial commit.

---
+++ main.py
@@ -0,0 +1,19 @@
+"""Docstring: A very short sentence explaining the function. < 79 characters.
+
+Additional information if required and more infos. Complete sentences please.
+"""
+
+import json
+
+__author__ = "6770541: Niels Heissel"
+__copyright__ = "Copyright 2017/2018 – EPR-Goethe-Uni"
+__credits__ = "Thanks Mom :)"
+__email__ = "nielsheissel99@googlemail.com"
+
+
+
+def main():
+    print("Let's start the program.")
+
+if __name__ == '__main__':
+    main()
